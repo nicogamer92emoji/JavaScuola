17603399f3bea071ab7261e4db7b51fa151e151f
nico gamer <daniele25052007@gmail.com>
2025-04-08 16:53:43 +0200

    inizio frame

---
+++ src/frames/JFrame1.java
@@ -0,0 +1,36 @@
+package frames;
+
+import javax.swing.JFrame;
+
+public class JFrame1 extends JFrame{
+    private final String titolo;
+    private final String testo;
+    private final int larghezza;
+    private final int altezza;
+
+    public JFrame1(String titolo, String testo, int larghezza, int altezza) {
+        this.titolo = titolo;
+        this.testo = testo;
+        this.larghezza = larghezza;
+        this.altezza = altezza;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public String getTesto() {
+        return testo;
+    }
+
+    public int getLarghezza() {
+        return larghezza;
+    }
+
+    public int getAltezza() {
+        return altezza;
+    }
+
+    
+    
+}
