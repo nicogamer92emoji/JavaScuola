b510d75ab95ea43535adf1d5e4dd2f9febd6a8e8
nico gamer <daniele25052007@gmail.com>
2025-04-14 19:48:11 +0200

    add CalcInterfaccia class for calculator GUI with basic operations

---
+++ src/CalcInterfaccia.java
@@ -0,0 +1,71 @@
+import javax.swing.*;
+
+public class CalcInterfaccia {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Calcolatrice");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setLayout(null);
+        frame.setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        panel.setBounds(0, 0, 400, 300);
+        frame.add(panel);
+        
+        JLabel label = new JLabel("N.1");
+        label.setBounds(10, 10, 50, 25);
+        panel.add(label);
+
+        JTextField textField1 = new JTextField(10);
+        textField1.setBounds(10, 30, 50, 25);
+        panel.add(textField1);
+        
+        JLabel label2 = new JLabel("Operatore");
+        label2.setBounds(70, 10, 100, 25);
+        panel.add(label2);
+        
+        String[] operatoriArray = {"+", "-", "*", "/"};
+        JComboBox<String> operatori = new JComboBox<>(operatoriArray);
+        operatori.setBounds(75, 30, 50, 25);
+        panel.add(operatori);
+
+        JLabel label3 = new JLabel("N.2");
+        label3.setBounds(150, 10, 50, 25);
+        panel.add(label3);
+        
+        JTextField textField2 = new JTextField(10);
+        textField2.setBounds(150, 30, 50, 25);
+        panel.add(textField2);
+
+        JTextField uguale = new JTextField("=");
+        uguale.setBounds(220, 30, 50, 25);
+        uguale.setEditable(false);
+        panel.add(uguale);
+
+        JButton calcolaButton = new JButton("Calcola");
+        calcolaButton.setBounds(280, 30, 100, 25);
+        panel.add(calcolaButton);
+
+        calcolaButton.addActionListener(e -> {
+                double num1 = Double.parseDouble(textField1.getText());
+                double num2 = Double.parseDouble(textField2.getText());
+                char operatore = operatori.getSelectedItem().toString().charAt(0);
+                double risultato;
+                
+                switch (operatore) {
+                    case '+' -> risultato = num1 + num2;
+                    case '-' -> risultato = num1 - num2;
+                    case '*' -> risultato = num1 * num2;
+                    case '/' -> risultato = num1 / num2;
+                    default -> {
+                        JOptionPane.showMessageDialog(frame, "Operatore non valido", "Errore", JOptionPane.ERROR_MESSAGE); return;
+                    }
+                }
+                
+                uguale.setText(String.valueOf("= " + risultato));
+        });
+
+        frame.setVisible(true);
+    }
+}
