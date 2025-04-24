0b10325f8db1cd603ce290cc0be3444205f3eac3
nicogamer94emoji <daniele25052007@gmail.com>
2025-04-24 12:43:04 +0200

    add Processi class to implement Runnable interface and demonstrate multithreading

--- src/Processi.java
+++ src/Processi.java
@@ -1,5 +1,16 @@
+public class Processi implements Runnable {
+    @Override
+    public void run() {
+        for(int i = 0; i < 10; i++) 
+            System.out.println("Thread " + i);
+    }
 
+    public static void main(String[] args) {
+        Processi p = new Processi();
 
-public class Processi {
+        p.run();
+        for (int i = 0; i < 10; i++) 
+            System.out.println("Main " + i);
+    }
 
 }
